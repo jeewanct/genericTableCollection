fcc9a36d160ec66b74722f3a8e950f97ba90ee76
Jeevan <jeevan.tiwari@one97.net>
2020-01-13 22:38:26 +0530

    Initial Commit

---
+++ GenericCollectionTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericCollectionTable
+//
+//  Created by jeevan tiwari on 13/01/20.
+//  Copyright © 2020 jeevan tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
